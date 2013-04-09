c1410c19440ab0409286ab7c3821bba698f5413f
Baho Utot <baho-utot@columbus.rr.com>
2013-04-09 11:59:04 -0400

    2013-04-09: Fixed dangling blobs

--- rpmbuild/SCRIPTS/configuration.sh
+++ rpmbuild/SCRIPTS/configuration.sh
@@ -3,28 +3,28 @@ set -o errexit	# exit if error
 set +h		# disable hashall
 shopt -s -o pipefail
 #	install locale files
-printf "%s\n" "Creating locale files"
-/usr/bin/localedef -i cs_CZ -f UTF-8 cs_CZ.UTF-8
-/usr/bin/localedef -i de_DE -f ISO-8859-1 de_DE
-/usr/bin/localedef -i de_DE@euro -f ISO-8859-15 de_DE@euro
-/usr/bin/localedef -i de_DE -f UTF-8 de_DE.UTF-8
-/usr/bin/localedef -i en_GB -f UTF-8 en_GB.UTF-8
-/usr/bin/localedef -i en_HK -f ISO-8859-1 en_HK
-/usr/bin/localedef -i en_PH -f ISO-8859-1 en_PH
-/usr/bin/localedef -i en_US -f ISO-8859-1 en_US
-/usr/bin/localedef -i en_US -f UTF-8 en_US.UTF-8
-/usr/bin/localedef -i es_MX -f ISO-8859-1 es_MX
-/usr/bin/localedef -i fa_IR -f UTF-8 fa_IR
-/usr/bin/localedef -i fr_FR -f ISO-8859-1 fr_FR
-/usr/bin/localedef -i fr_FR@euro -f ISO-8859-15 fr_FR@euro
-/usr/bin/localedef -i fr_FR -f UTF-8 fr_FR.UTF-8
-/usr/bin/localedef -i it_IT -f ISO-8859-1 it_IT
-/usr/bin/localedef -i it_IT -f UTF-8 it_IT.UTF-8
-/usr/bin/localedef -i ja_JP -f EUC-JP ja_JP
-/usr/bin/localedef -i ru_RU -f KOI8-R ru_RU.KOI8-R
-/usr/bin/localedef -i ru_RU -f UTF-8 ru_RU.UTF-8
-/usr/bin/localedef -i tr_TR -f UTF-8 tr_TR.UTF-8
-/usr/bin/localedef -i zh_CN -f GB18030 zh_CN.GB18030
+#printf "%s\n" "Creating locale files"
+#/usr/bin/localedef -i cs_CZ -f UTF-8 cs_CZ.UTF-8
+#/usr/bin/localedef -i de_DE -f ISO-8859-1 de_DE
+#/usr/bin/localedef -i de_DE@euro -f ISO-8859-15 de_DE@euro
+#/usr/bin/localedef -i de_DE -f UTF-8 de_DE.UTF-8
+#/usr/bin/localedef -i en_GB -f UTF-8 en_GB.UTF-8
+#/usr/bin/localedef -i en_HK -f ISO-8859-1 en_HK
+#/usr/bin/localedef -i en_PH -f ISO-8859-1 en_PH
+#/usr/bin/localedef -i en_US -f ISO-8859-1 en_US
+#/usr/bin/localedef -i en_US -f UTF-8 en_US.UTF-8
+#/usr/bin/localedef -i es_MX -f ISO-8859-1 es_MX
+#/usr/bin/localedef -i fa_IR -f UTF-8 fa_IR
+#/usr/bin/localedef -i fr_FR -f ISO-8859-1 fr_FR
+#/usr/bin/localedef -i fr_FR@euro -f ISO-8859-15 fr_FR@euro
+#/usr/bin/localedef -i fr_FR -f UTF-8 fr_FR.UTF-8
+#/usr/bin/localedef -i it_IT -f ISO-8859-1 it_IT
+#/usr/bin/localedef -i it_IT -f UTF-8 it_IT.UTF-8
+#/usr/bin/localedef -i ja_JP -f EUC-JP ja_JP
+#/usr/bin/localedef -i ru_RU -f KOI8-R ru_RU.KOI8-R
+#/usr/bin/localedef -i ru_RU -f UTF-8 ru_RU.UTF-8
+#/usr/bin/localedef -i tr_TR -f UTF-8 tr_TR.UTF-8
+#/usr/bin/localedef -i zh_CN -f GB18030 zh_CN.GB18030
 #	install ld cache
 /sbin/ldconfig
 #	enable shadowed passwords and group passwords
